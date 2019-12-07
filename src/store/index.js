cc2de12eed67a65ac86add3d41b9ee0dcbb00cc2
Gloria Morales <gmoraleslondono@gmail.com>
2019-12-07 14:37:28 +0100

    Add set vuex store file. State, actions and mutations

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,43 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  // data in the component
   state: {
+    choices: ['rock', 'paper', 'scissors'],
+    computerChoice: '',
+    userChoice: '',
+    isUserWinner: false,
+    showResult: false,
   },
+  // methods that changes the state
   mutations: {
+    setIsUserWinner(state, choices) {
+      let isUserWinner;
+      if (choices.userChoice === 'rock' && choices.computerChoice === 'scissor') {
+        isUserWinner = true;
+      } else if (choices.userChoice === 'paper' && choices.computerChoice === 'rock') {
+        isUserWinner = true;
+      } else if (choices.userChoice === 'scissors' && choices.computerChoice === 'paper') {
+        isUserWinner = true;
+      } else if (choices.userChoice === choices.computerChoice) {
+        isUserWinner = false;
+      } else {
+        isUserWinner = false;
+      }
+      // return this.isUserWinner;
+      state.isUserWinner = isUserWinner;
+    },
   },
+  // methods in the component
   actions: {
+    playGame({ commit, state }, userSelection) {
+      const computerChoice = state.choices[Math.floor(Math.random() * state.choices.length)];
+      const userChoice = userSelection;
+      console.log(userChoice, computerChoice, state.isUserWinner);
+      commit('setIsUserWinner', { computerChoice, userChoice });
+      // this.checkWinner();
+      // this.showResult = !this.showResult;
+    },
   },
   modules: {
   },
